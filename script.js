0d36fc0f77a1a5026c2b752714de2ed4f2a57e6a
dannnluvs <110661011+dannnluvs@users.noreply.github.com>
2025-03-29 12:17:52 +0800

    Final

---
+++ script.js
@@ -0,0 +1,8 @@
+function alertButton() {
+
+    let fullname = document.getElementById("txtFullName").value;
+    let age = document.getElementById("txtAge").value;
+    let address = document.getElementById("txtAddress").value;
+
+    alert("Your FullName is: " + fullname + " " + "Your age is: " + age + " " + "Your address is: " + address);
+}
